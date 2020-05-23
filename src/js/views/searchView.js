76dd9f0b709841fe19d4730e259fe6bc00a1150a
danvarga1985 <danvarga1985@gmail.com>
2020-05-23 07:30:59 +0200

    searchView: limitRecipeTitle()

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,4 +1,6 @@
-import { elements } from './base';
+import {
+    elements
+} from './base';
 
 export const getInput = () => elements.searchInput.value;
 
@@ -10,6 +12,22 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 }
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, curr) => {
+            if (acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+            //return updates the accumuator
+            return acc + curr.length;
+        }, 0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -18,7 +36,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
